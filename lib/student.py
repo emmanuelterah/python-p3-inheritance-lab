596a9d1018c6bf820b678c2c4ba83bffafb80bec
emmanuelterah <emmanuelterah98@gmail.com>
2024-03-11 03:53:32 +0300

    Completed lab assignment

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,14 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, information):
+        self.knowledge.append(information)
+
+my_student = Student("My", "Student")
+my_student.first_name
+my_student.last_name
--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,17 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name, knowledge=None, list=None):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Initial knowledge 1", "Initial knowledge 2"]
+        self.list = list if list else []
+
     def teach(self):
-        pass
+        if self.knowledge:
+            return list(self.knowledge)[0]
+        else:
+            return None
+        
+my_teacher = Teacher("My", "Teacher")
+my_teacher.first_name
+my_teacher.last_name
